efe2855b3332265002c3e5c3c41e63d0b03a4dc3
alehuo <aleksi.huotala@helsinki.fi>
2018-02-16 01:03:26 +0200

    Added Product category seed

--- src/db/seeds/development/products.js
+++ src/db/seeds/development/products.js
@@ -1,3 +1,89 @@
 exports.seed = function(knex, Promise) {
-
+    return knex('PRODGROUP')
+        .del()
+        .then(() => {
+            return knex('PRODGROUP').insert([
+                {
+                    pgrpid: 0,
+                    descr: 'DEFAULT GROUP, NO DEFINITION'
+                },
+                {
+                    pgrpid: 1,
+                    descr: 'Drinks, soda with bottle return'
+                },
+                {
+                    pgrpid: 2,
+                    descr: 'Drinks, soda with can return'
+                },
+                {
+                    pgrpid: 3,
+                    descr: 'Drinks, juices'
+                },
+                {
+                    pgrpid: 4,
+                    descr: 'Drinks, soda without can return'
+                },
+                {
+                    pgrpid: 10,
+                    descr: 'Sweets, chocolate'
+                },
+                {
+                    pgrpid: 11,
+                    descr: 'Sweets, other'
+                },
+                {
+                    pgrpid: 12,
+                    descr: 'Sweets, ice creams'
+                },
+                {
+                    pgrpid: 20,
+                    descr: 'Food, pizzas'
+                },
+                {
+                    pgrpid: 21,
+                    descr: 'Food, noodles'
+                },
+                {
+                    pgrpid: 22,
+                    descr: 'Food, dairy'
+                },
+                {
+                    pgrpid: 23,
+                    descr: 'Food, salty snacks (beef jerky, potato chips etc.'
+                },
+                {
+                    pgrpid: 24,
+                    descr: 'Food, other (meat pies etc.)'
+                },
+                {
+                    pgrpid: 30,
+                    descr: 'Hot liquids, coffee'
+                },
+                {
+                    pgrpid: 31,
+                    descr: 'Hot liquids, tea'
+                },
+                {
+                    pgrpid: 32,
+                    descr: 'Hot liquids, hot chocolate'
+                },
+                {
+                    pgrpid: 80,
+                    descr: 'Bottle return (negative price)'
+                },
+                {
+                    pgrpid: 81,
+                    descr: 'Can return (negative price)'
+                },
+                {
+                    pgrpid: 90,
+                    descr:
+                        'Seasonal products (kinder-eggs, chocolate santa claus etc'
+                },
+                {
+                    pgrpid: 65535,
+                    descr: 'OBSOLETED (do not show)'
+                }
+            ]);
+        });
 };
