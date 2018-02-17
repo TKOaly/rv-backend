6c45195b1cd79c66ae4acdad0d49b6b83879081c
laazz <lasse.vallius@helsinki.fi>
2018-02-17 09:08:49 +0200

    Update Docker startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-./node_modules/knex/bin/cli.js migrate:latest --env $NODE_ENV
-./node_modules/knex/bin/cli.js seed:run --env $NODE_ENV
+if [ $NODE_ENV != "production" ]
+then
+    ./node_modules/knex/bin/cli.js migrate:rollback --env $NODE_ENV
+    ./node_modules/knex/bin/cli.js migrate:latest --env $NODE_ENV
+    ./node_modules/knex/bin/cli.js seed:run --env $NODE_ENV
+else
+    ./node_modules/knex/bin/cli.js migrate:latest --env $NODE_ENV
+fi
+
 npm start
