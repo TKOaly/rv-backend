707d5b7d702467fcf431578f5c92b41d86b2430d
laazz <lasse.vallius@helsinki.fi>
2018-03-14 13:44:36 +0200

    Tests for admin login

---
+++ test/admin/routes.adminauth.test.js
@@ -0,0 +1,84 @@
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test secret';
+process.env.JWT_ADMIN_SECRET = 'admin test secret';
+
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const server = require('../../src/app');
+const request = chai.request(server);
+const knex = require('../../src/db/knex');
+const jwt = require('../../src/jwt/token');
+
+describe('routes: admin authentication', () => {
+
+    beforeEach((done) => {
+        knex.migrate.rollback()
+            .then(() => {
+                knex.migrate.latest()
+                    .then(() => {
+                        knex.seed.run()
+                            .then(() => {
+                                done();
+                            });
+                    });
+            });
+    });
+
+    afterEach((done) => {
+        knex.migrate.rollback()
+            .then(() => {
+                done();
+            });
+    });
+
+    describe('Admin authentication', () => {
+        it('logging in with admin role should work', (done) => {
+            chai.request(server)
+                .post('/api/v1/admin/authenticate')
+                .send({
+                    username: 'admin_user',
+                    password: 'admin123'
+                })
+                .end((err, res) => {
+                    should.not.exist(err);
+
+                    var decoded = jwt.verify(res.body.access_token, process.env.JWT_ADMIN_SECRET);
+                    expect(decoded.data.username).to.equal('admin_user');
+                    done();
+                });
+        });
+
+        it('admin tokens should not be signed with the same key as user tokens', (done) => {
+            chai.request(server)
+                .post('/api/v1/admin/authenticate')
+                .send({
+                    username: 'admin_user',
+                    password: 'admin123'
+                })
+                .end((err, res) => {
+                    should.not.exist(err);
+                    var decoded = jwt.verify(res.body.access_token, process.env.JWT_SECRET);
+                    expect(decoded).to.equal(null);
+                    done();
+                });
+        });
+
+        it('only admins should be able to authenticate', (done) => {
+            chai.request(server)
+                .post('/api/v1/admin/authenticate')
+                .send({
+                    username: 'normal_user',
+                    password: 'hunter2'
+                })
+                .end((err, res) => {
+                    should.exist(err);
+                    res.status.should.equal(403);
+                    done();
+                });
+        });
+    });
+});
