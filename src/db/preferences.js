e423a82321c7f690478ff97218c9e4454968e1d4
Mitja Karhusaari <mitja@karhusaari.me>
2020-10-10 20:49:22 +0300

    Implement mechanism for global preferences
    Global preferences are stored in a database table named
    `PREFERENCES`.  Each preference is identified by a textual
    `key` and has a textual value.  Format of the textual value
    is preference specific.
    
    API for reading and writing preferences, as well as definitions
    of all preferences, are provided in `src/db/preferences.js`.
    In addition to the `key` and an optional `default` value, each
    preference definition can provide functions for serializing and
    deserializing the preference value.

---
+++ src/db/preferences.js
@@ -0,0 +1,51 @@
+const knex = require('./knex');
+
+module.exports.getPreference = async (preference) => {
+    const data = await knex('PREFERENCES')
+        .where({ key: preference.key })
+        .first('value');
+
+    if (data === undefined) {
+        if (preference.default !== undefined) {
+            return preference.default;
+        } else {
+            return null;
+        }
+    } else {
+        if (preference.deserialize) {
+            return preference.deserialize(data.value);
+        } else {
+            return data.value;
+        }
+    }
+};
+
+module.exports.setPreference = async (preference, value) => {
+    const data = await knex('PREFERENCES')
+        .where({ key: preference.key })
+        .first();
+
+    if (preference.serialize) {
+        value = preference.serialize(value);
+    }
+
+    if (data === undefined) {
+        await knex('PREFERENCES')
+            .insert({ key: preference.key, value });
+    } else {
+        await knex('PREFERENCES')
+            .update({ value })
+            .where({ key: preference.key });
+    }
+};
+
+const floatPreference = {
+    serialize: (value) => String(value),
+    deserialize: (str) => parseFloat(str)
+};
+
+module.exports.GLOBAL_DEFAULT_MARGIN = {
+    ... floatPreference,
+    key: 'GLOBAL_DEFAULT_MARGIN',
+    default: 0.05
+};
