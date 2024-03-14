59556bc2cd5808383dd24c2a89c4147df7071e8f
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2024-03-14 20:46:38 +0200

    Also drop bgcolor and fgcolor

--- src/db/migrations/20240314202750_role_cleanup.js
+++ src/db/migrations/20240314202750_role_cleanup.js
@@ -1,5 +1,7 @@
 export const up = async (knex) => {
 	await knex.raw('ALTER TABLE "ROLE" DROP COLUMN "buzzerlimit"');
+	await knex.raw('ALTER TABLE "ROLE" DROP COLUMN "bgcolor"');
+	await knex.raw('ALTER TABLE "ROLE" DROP COLUMN "fgcolor"');
 };
 
 export const down = async () => {};
--- src/db/seeds/seeddata/ROLE.js
+++ src/db/seeds/seeddata/ROLE.js
@@ -2,25 +2,17 @@ export const roles = [
 	{
 		roleid: 1,
 		role: 'ADMIN',
-		fgcolor: 37,
-		bgcolor: 40,
 	},
 	{
 		roleid: 2,
 		role: 'USER1',
-		fgcolor: 37,
-		bgcolor: 40,
 	},
 	{
 		roleid: 3,
 		role: 'USER2',
-		fgcolor: 31,
-		bgcolor: 47,
 	},
 	{
 		roleid: 7,
 		role: 'INACTIVE',
-		fgcolor: 37,
-		bgcolor: 40,
 	},
 ];
