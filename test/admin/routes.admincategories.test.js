ec0742d6d681523ef2d788a6687eabd1090df37c
Tuupertunut <tuupertunut@outlook.com>
2024-03-25 06:21:56 +0200

    Add tests for category deletion

--- test/admin/routes.admincategories.test.js
+++ test/admin/routes.admincategories.test.js
@@ -217,14 +217,24 @@ describe('routes: admin categories', () => {
 	});
 
 	describe('deleting a category', () => {
-		it('should fail with a nonexisting category', async () => {
+		it('should delete the category', async () => {
 			const res = await chai
 				.request(app)
-				.delete('/api/v1/admin/categories/9999')
+				.delete('/api/v1/admin/categories/20')
 				.set('Authorization', 'Bearer ' + adminToken);
 
-			expect(res.status).to.equal(404);
-			expect(res.body.error_code).to.equal('not_found');
+			expect(res.status).to.equal(200);
+
+			const deletedCategory = await categoryStore.findById(20);
+			expect(deletedCategory).to.not.exist;
+
+			const post_res = await chai
+				.request(app)
+				.get('/api/v1/admin/categories/20')
+				.set('Authorization', 'Bearer ' + adminToken);
+
+			expect(post_res.status).to.equal(404);
+			expect(post_res.body.error_code).to.equal('not_found');
 		});
 
 		it('should return the deleted category and moved items', async () => {
@@ -266,6 +276,41 @@ describe('routes: admin categories', () => {
 			post_res.body.products
 				.filter((prod) => initial_items.indexOf(prod.barcode) !== -1)
 				.forEach((prod) => expect(prod.category.categoryId).to.not.equal(20));
+		});
+
+		it('should prevent adding new products to the category', async () => {
+			const res = await chai
+				.request(app)
+				.delete('/api/v1/admin/categories/20')
+				.set('Authorization', 'Bearer ' + adminToken);
+
+			expect(res.status).to.equal(200);
+
+			const post_res = await chai
+				.request(app)
+				.post('/api/v1/admin/products')
+				.set('Authorization', 'Bearer ' + adminToken)
+				.send({
+					barcode: '575757575757',
+					name: 'Epäonnistuvaa ruokaa',
+					categoryId: 20,
+					buyPrice: 367,
+					sellPrice: 370,
+					stock: 1,
+				});
+
+			expect(post_res.status).to.equal(400);
+			expect(post_res.body.error_code).to.equal('invalid_reference');
+		});
+
+		it('should fail with a nonexisting category', async () => {
+			const res = await chai
+				.request(app)
+				.delete('/api/v1/admin/categories/9999')
+				.set('Authorization', 'Bearer ' + adminToken);
+
+			expect(res.status).to.equal(404);
+			expect(res.body.error_code).to.equal('not_found');
 		});
 
 		it('should fail with the default category', async () => {
