c58ce32b8d4bfc4e9a0707d41220f539d9488bc0
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2025-03-28 14:50:53 +0200

    Fix registration tests missing RV_TERMINAL_SECRET header

--- test/routes.register.test.ts
+++ test/routes.register.test.ts
@@ -28,33 +28,36 @@ describe('routes: register', () => {
 
 	describe('Trying to register with missing field or bad password etc', () => {
 		it('Request should not have missing keys', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				// empty string
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					// empty string
+				});
 
 			expect(res.status).to.equal(400);
 			expect(res.body.error_code).to.equal('bad_request');
 		});
 
 		it('Username should not be empty', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				username: '',
-				password: 'test',
-				fullName: 'm.erkki',
-				email: 'erkki@testi.com',
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					username: '',
+					password: 'test',
+					fullName: 'm.erkki',
+					email: 'erkki@testi.com',
+				});
 
 			expect(res.status).to.equal(400);
 			expect(res.body.error_code).to.equal('bad_request');
 		});
 
 		it('User password should not be empty', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				username: 'test',
-				password: '',
-				fullName: 'm.erkki',
-				email: 'erkki@testi.com',
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					username: 'test',
+					password: '',
+					fullName: 'm.erkki',
+					email: 'erkki@testi.com',
+				});
 
 			expect(res.status).to.equal(400);
 			expect(res.body.error_code).to.equal('bad_request');
@@ -63,24 +66,26 @@ describe('routes: register', () => {
 
 	describe('Usernames and Emails should be uniques', () => {
 		it('Username should be unique', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				username: 'normal_user',
-				password: 'test',
-				fullName: 'm.erkki',
-				email: 'erkki@testi.com',
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					username: 'normal_user',
+					password: 'test',
+					fullName: 'm.erkki',
+					email: 'erkki@testi.com',
+				});
 
 			expect(res.status).to.equal(409);
 			expect(res.body.error_code).to.equal('identifier_taken');
 		});
 
 		it('Email should be unique', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				username: 'test',
-				password: 'test',
-				fullName: 'm.erkki',
-				email: 'user@example.com',
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					username: 'test',
+					password: 'test',
+					fullName: 'm.erkki',
+					email: 'user@example.com',
+				});
 
 			expect(res.status).to.equal(409);
 			expect(res.body.error_code).to.equal('identifier_taken');
@@ -89,7 +94,7 @@ describe('routes: register', () => {
 
 	describe('User should be able to register to service', () => {
 		it('With all required fields user should be registered to service', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
+			const res = await chai.request(app).post('/api/v1/register').set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
 				username: 'test',
 				password: 'test',
 				fullName: 'm.erkki',
@@ -100,7 +105,7 @@ describe('routes: register', () => {
 		});
 
 		it('Registering should create a new user to the database', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
+			const res = await chai.request(app).post('/api/v1/register').set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
 				username: 'abc',
 				password: 'def',
 				fullName: 'No Body',
@@ -114,12 +119,13 @@ describe('routes: register', () => {
 		});
 
 		it('It should return the new user', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				username: 'abc',
-				password: 'def',
-				fullName: 'No Body',
-				email: 'person@email.com',
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					username: 'abc',
+					password: 'def',
+					fullName: 'No Body',
+					email: 'person@email.com',
+				});
 
 			expect(res.status).to.equal(201);
 
@@ -129,12 +135,13 @@ describe('routes: register', () => {
 		});
 
 		it('New user should have role USER1', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				username: 'abc',
-				password: 'def',
-				fullName: 'No Body',
-				email: 'person@email.com',
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					username: 'abc',
+					password: 'def',
+					fullName: 'No Body',
+					email: 'person@email.com',
+				});
 
 			expect(res.status).to.equal(201);
 
@@ -143,12 +150,13 @@ describe('routes: register', () => {
 		});
 
 		it('New user should have no money', async () => {
-			const res = await chai.request(app).post('/api/v1/register').send({
-				username: 'abc',
-				password: 'def',
-				fullName: 'No Body',
-				email: 'person@email.com',
-			});
+			const res = await chai.request(app).post('/api/v1/register').
+				set('RV-Terminal-Secret', process.env.RV_TERMINAL_SECRET).send({
+					username: 'abc',
+					password: 'def',
+					fullName: 'No Body',
+					email: 'person@email.com',
+				});
 
 			expect(res.status).to.equal(201);
 
