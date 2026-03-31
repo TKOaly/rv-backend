2fc0f20a1f094b33089f56d9cd7ab186be9d2109
Juuso Kosonen <kosonenjusu@gmail.com>
2026-03-31 19:35:09 +0300

    added v1 functionaly back

--- src/routes/authUtils.ts
+++ src/routes/authUtils.ts
@@ -106,3 +106,84 @@ export const authenticateUser =
 			});
 		}
 	};
+
+export const authenticateUserRfidOld =
+	(requiredRole = null, tokenSecret = process.env.JWT_SECRET) =>
+	async (req, res) => {
+		const body = req.body;
+		const rfid = body.rfid;
+		let loggedInFromRvTerminal = false;
+		if (body.rvTerminalSecret === process.env.RV_TERMINAL_SECRET) {
+			loggedInFromRvTerminal = true;
+		} else {
+			logger.warn('Rfid login failed, rv_terminal_secret not included');
+			res.status(401).json({
+				error_code: 'invalid_credentials',
+				message: 'Invalid rfid',
+			});
+			return;
+		}
+
+		const user = await userStore.findByRfid(rfid);
+		if (user) {
+			if (verifyRole(requiredRole, user.role)) {
+				logger.info('User %s logged in as role %s', user.username, requiredRole);
+				res.status(200).json({
+					accessToken: jwt.sign({ userId: user.userId, loggedInFromRvTerminal }, tokenSecret),
+				});
+			} else {
+				logger.warn('User %s is not authorized to login as role %s', user.username, requiredRole);
+				res.status(403).json({
+					error_code: 'not_authorized',
+					message: 'Not authorized',
+				});
+			}
+		} else {
+			logger.warn('Failed to login with rfid');
+			res.status(401).json({
+				error_code: 'invalid_credentials',
+				message: 'Invalid rfid',
+			});
+		}
+	};
+
+export const authenticateUserOld =
+	(requiredRole = null) =>
+	async (req, res) => {
+		const body = req.body;
+		const username = body.username;
+		const password = body.password;
+		let loggedInFromRvTerminal = false;
+		if (body.rvTerminalSecret === process.env.RV_TERMINAL_SECRET) {
+			loggedInFromRvTerminal = true;
+		}
+		const user = await userStore.findByUsername(username);
+		if (user) {
+			if (password != undefined && (await userStore.verifyPassword(password, user.passwordHash))) {
+				if (verifyRole(requiredRole, user.role)) {
+					logger.info('User %s logged in with role %s', user.username, user.role);
+					res.status(200).json({
+						accessToken: jwt.sign({ userId: user.userId, loggedInFromRvTerminal }, process.env.JWT_SECRET),
+					});
+				} else {
+					logger.warn('User %s is not authorized to login as role %s', user.username, requiredRole);
+					res.status(403).json({
+						error_code: 'not_authorized',
+						message: 'Not authorized',
+					});
+				}
+			} else {
+				logger.warn('Failed to login with username and password. Username was %s', username);
+				res.status(401).json({
+					error_code: 'invalid_credentials',
+					message: 'Invalid username or password',
+				});
+			}
+		} else {
+			logger.warn('Failed to login with username and password. Username was %s', username);
+			res.status(401).json({
+				error_code: 'invalid_credentials',
+				message: 'Invalid username or password',
+			});
+		}
+	};
--- src/routes/old.ts
+++ src/routes/old.ts
@@ -1,15 +1,9 @@
 import express from 'express';
+import { authenticateUserOld, authenticateUserRfidOld } from './authUtils.js';
 
 const router = express.Router();
 
-router.post('/v1/authenticate', async (req, res) => { res.status(500).json({
-    error_code: 'resource_gone',
-    message: 'This resource has been permanently removed. Use insteed /api/v2/authenticate',
-});});
-
-router.post('/v1/authenticate/rfid', async (req, res) => { res.status(500).json({
-    error_code: 'resource_gone',
-    message: 'This resource has been permanently removed. Use insteed /api/v2/authenticate/rfid',
-});});
+router.post('/v1/authenticate', authenticateUserOld());
+router.post('/v1/authenticate/rfid', authenticateUserRfidOld());
 
 export default router;
