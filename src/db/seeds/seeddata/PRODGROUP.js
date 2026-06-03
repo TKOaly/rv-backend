3d609e2eb57d7267300e4dd6c872d57ea4476411
Juuso Kosonen <kosonenjusu@gmail.com>
2026-06-03 23:57:34 +0300

    update product categories

--- src/db/seeds/seeddata/PRODGROUP.js
+++ src/db/seeds/seeddata/PRODGROUP.js
@@ -1,79 +1,95 @@
 export const prodgroups = [
 	{
 		pgrpid: 0,
-		descr: 'DEFAULT GROUP, NO DEFINITION',
+		descr: 'DEFAULT GROUP, NO DEFINITION'
 	},
 	{
 		pgrpid: 1,
-		descr: 'Drinks, soda with bottle return',
+		descr: 'Drinks, can with return'
 	},
 	{
 		pgrpid: 2,
-		descr: 'Drinks, soda with can return',
+		descr: 'Drinks, plastic bottle with return'
 	},
 	{
 		pgrpid: 3,
-		descr: 'Drinks, juices',
+		descr: 'Drinks, can without return'
 	},
 	{
 		pgrpid: 4,
-		descr: 'Drinks, soda without can return',
+		descr: 'Drinks, plastic bottle without return'
+	},
+	{
+		pgrpid: 5,
+		descr: 'Drinks, other'
 	},
 	{
 		pgrpid: 10,
-		descr: 'Sweets, chocolate',
+		descr: 'Sweets, chocolate'
 	},
 	{
 		pgrpid: 11,
-		descr: 'Sweets, other',
+		descr: 'Sweets, candy'
 	},
 	{
 		pgrpid: 12,
-		descr: 'Sweets, ice creams',
+		descr: 'Sweets, ice creams'
+	},
+	{
+		pgrpid: 13,
+		descr: 'Sweets, other'
 	},
 	{
 		pgrpid: 20,
-		descr: 'Food, pizzas',
+		 descr: 'Food, frozen'
 	},
 	{
 		pgrpid: 21,
-		descr: 'Food, noodles',
+		descr: 'Food, noodles & meal cups'
 	},
 	{
 		pgrpid: 22,
-		descr: 'Food, dairy',
+		descr: 'Food, protein & snack bars'
 	},
 	{
 		pgrpid: 23,
-		descr: 'Food, salty snacks (beef jerky, potato chips etc.',
+		descr: 'Food, salty snacks (beef jerky, potato chips etc.'
 	},
 	{
 		pgrpid: 24,
-		descr: 'Food, other (meat pies etc.)',
+		descr: 'Food, salty snacks'
+	},
+	{
+		pgrpid: 25,
+		descr: 'Food, other'
 	},
 	{
 		pgrpid: 30,
-		descr: 'Hot liquids, coffee',
+		descr: 'Hot liquids, coffee'
 	},
 	{
 		pgrpid: 31,
-		descr: 'Hot liquids, tea',
+		descr: 'Hot liquids, tea'
 	},
 	{
 		pgrpid: 32,
-		descr: 'Hot liquids, hot chocolate',
+		descr: 'Hot liquids, hot chocolate'
 	},
 	{
 		pgrpid: 80,
-		descr: 'Bottle return (negative price)',
+		descr: 'Bottle return (negative price)'
 	},
 	{
 		pgrpid: 81,
-		descr: 'Can return (negative price)',
+		descr: 'Can return (negative price)'
 	},
 	{
 		pgrpid: 90,
 		descr: 'Seasonal products (kinder-eggs, chocolate santa claus etc',
+	},
+	{
+		pgrpid: 99,
+		descr: 'Miscellaneous'
 	},
 	{
 		pgrpid: 65535,
